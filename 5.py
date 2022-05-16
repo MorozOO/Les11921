93ffb52d9dcaf94ba7bb9e14fb965207c610f47a
MorozOO <Vx7nneN8508B>
2022-05-16 14:34:32 +0300

    lesson5

---
+++ 5.py
@@ -0,0 +1,48 @@
+import requests
+# class Computer:
+#     def __init__(self,model,*args,**kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.model = model
+#         self.memory = 128
+#     def calculate(self):
+#         print("Calculating...")
+# class Display:
+#     def __init__(self,*args,**kwargs):
+#         super().__init__(*args,**kwargs)
+#         self.resolution = "4k"
+#     def display(self):
+#         print("I display the image on the screen...")
+# class Phone:
+#     def call(self):
+#         print("calling...")
+# class SmartPhone(Display,Computer,Phone):
+#     def printInfo(self):
+#         print(self.model)
+#         print(self.resolution)
+#         print(self.memory)
+# iphone = SmartPhone(model = "Last")
+# iphone.printInfo()
+# iphone.calculate()
+# iphone.display()
+# iphone.call()
+
+
+# class Class1:
+#     var = 20
+#     def __init__(self):
+#         self.var=10
+# class Class2(Class1):
+#     def __init__(self):
+#         print(self.var)
+#         super().__init__()
+#         print(self.var)
+#         print(super().var)
+# obj = Class2()
+# class Hello:
+#     def __init__(self):
+#         print("Hello")
+# class HelloWorld(Hello):
+#     def __init__(self):
+#         super().__init__()
+#         print("World")
+# helW = HelloWorld()
