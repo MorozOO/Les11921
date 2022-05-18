a9df2daf5a5659ddd89c1ecabeea5a8c2740c4ac
MorozOO <Vx7nneN8508B>
2022-05-18 16:21:30 +0300

    Warning

--- 7.py
+++ 7.py
@@ -39,14 +39,20 @@
 # fir = 10
 # checker(fir)
 
-class BuildingError(Exception):
-    def __str__(self):
-        return f"With so much material the house cannot be built!"
-def check_material(amount_of_material, limit_value):
-        if amount_of_material > limit_value:
-            return "enough material"
-        else:
-            raise BuildingError(amount_of_material)
+# class BuildingError(Exception):
+#     def __str__(self):
+#         return f"With so much material the house cannot be built!"
+# def check_material(amount_of_material, limit_value):
+#         if amount_of_material > limit_value:
+#             return "enough material"
+#         else:
+#             raise BuildingError(amount_of_material)
+#
+# materials = 123
+# check_material(materials,300)
+import warnings
+warnings.simplefilter("ignore",SyntaxWarning)
+warnings.simplefilter("always",ImportWarning)
 
-materials = 123
-check_material(materials,300)
+warnings.warn("Warning, no code here", SyntaxWarning)
+warnings.warn("Warning, module not import", ImportWarning)
