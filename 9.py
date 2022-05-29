21ffac6738877cc891bfcb605e51238fd75e3999
MorozOO <Vx7nneN8508B>
2022-05-29 22:15:28 +0300

    lesson 9 logs

---
+++ 9.py
@@ -0,0 +1,29 @@
+# import  logging
+# x=  5
+# y = 8
+# z= 9
+# print(f"x = {x} y = {y} z = {z}")
+# print("x = %x y = {y} z = {z}")
+# print("x = {x} y = {y} z = {z}".format(x=x,y=y,z=z))
+# # my_file = open("BabyFile.txt", "w+")
+# # my_file.write("Привет, файл!")
+# # my_file.close()
+#
+#
+# # logging.basicConfig(level=logging.DEBUG, filename="logs.log",filemode="w",format="We have next logging massege:%(asctime)s%(levelname)s - %(message)s")
+# # try:
+# #     print(5/0)
+# # except ZeroDivisionError:
+# #     # print("ZeroDivisionError")
+# #     logging.debug("debug")
+# #     logging.info("info")
+# #     logging.warning("warning")
+# #     logging.error("error")
+# #     logging.critical("critical")
+# #
+# #     logging.debug("debug2")
+# #     logging.info("info2")
+
+# if 2+2 ==5:
+#     print("done")
+assert  2+2 ==5, "not true"
