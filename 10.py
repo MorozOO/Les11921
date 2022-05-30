043aa78af9b3562fd537730eb2e82d60d7d0e32e
MorozOO <Vx7nneN8508B>
2022-05-30 16:24:11 +0300

    start code parser

--- 10.py
+++ 10.py
@@ -1,4 +1,7 @@
 import requests
-response = requests.get("https://httpbin.org/get")
-print(response.content)
-print(f"Datatype - {type(response.content)}")
+responce = requests.get("https://coinmarketcap.com")
+responce_text = (responce.text)
+responce_parse = responce_text.split("<span>")
+for parse_elem_1 in responce_parse:
+    if parse_elem_1.startswith("$"):
+        print(parse_elem_1)
